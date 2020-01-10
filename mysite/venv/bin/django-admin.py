9f9d69fa5ac1c3d06ae3afc7710b2d3e7c56d5d0
MackRoe <elaine.music@students.makeschool.com>
2020-01-09 16:18:27 -0800

    take 10

--- mysite/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/elaine/school-projects/WinterIntensive2020/MakinMine/mysite/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
