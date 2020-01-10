b5ed7f36f598897b1172097a0e1d655e597ed50c
MackRoe <elaine.music@students.makeschool.com>
2020-01-10 11:20:19 -0800

    modify settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj-database-url  # add this to the top of your file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,6 +131,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # recipes app settings
 RECIPE_PAGE_TITLE_MAX_LENGTH = 600
 
-import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
+
 DATABASES[‘default’].update(db_from_env)
